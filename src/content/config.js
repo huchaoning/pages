1c4af399a3da8424defaa499e780a448eaa77962
vxyi <vxyi@qq.com>
2026-03-05 18:09:48 +0800

    中文测试

--- src/content/config.js
+++ src/content/config.js
@@ -2,9 +2,11 @@ import { defineCollection, z } from "astro:content";
 
 const postSchema = z.object({
   title: z.string(),
+  title_zh: z.string().optional(),
   released: z.coerce.date().optional(),
   abstract: z.string().optional(),
   description: z.string().optional(),
+  lang: z.string().optional(),
 });
 
 export const collections = {
